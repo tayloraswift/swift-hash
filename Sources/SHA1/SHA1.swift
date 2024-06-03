44ebe381f1bb128eb4ef652ce85d6cfa1f1a7870
taylorswift <kelvin13ma@gmail.com>
2024-06-03 03:38:22 +0000

    clarify SHA1 type cannot yet compute the hash

--- Sources/SHA1/SHA1.swift
+++ Sources/SHA1/SHA1.swift
@@ -1,13 +1,16 @@
+/// The value of a [SHA-1](https://en.wikipedia.org/wiki/SHA-1) hash.
+///
+/// Currently this type can only represent a SHA-1 hash value; it cannot yet compute the hash.
 @frozen public
 struct SHA1:Equatable, Hashable, Sendable
 {
-    @usableFromInline internal
+    @usableFromInline
     typealias Storage = (UInt32, UInt32, UInt32, UInt32, UInt32)
 
-    @usableFromInline internal
+    @usableFromInline
     var buffer:InlineBuffer<Storage>
 
-    @inlinable internal
+    @inlinable
     init(buffer:InlineBuffer<Storage>)
     {
         self.buffer = buffer
